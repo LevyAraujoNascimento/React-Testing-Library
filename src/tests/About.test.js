f69a51d0e5f27f1aa4e267055a620a4b033cd33c
Levy Araujo <levy4raujo@gmail.com>
2023-02-01 17:25:35 -0300

    correção Lint 2º requisito

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { act } from 'react-dom/test-utils';
-import renderWithRouter from '../renderWithRouter';
-import App from '../App';
 import { About } from '../pages';
 
 describe('Teste do Componente About', () => {
   it('Deve renderizar o componente About', () => {
     render(<About />);
 
-    const title = screen.getByRole('heading', { level:2, name: 'About Pokédex' });
+    const title = screen.getByRole('heading', { level: 2, name: 'About Pokédex' });
     expect(title).toBeInTheDocument();
   });
 
@@ -28,4 +24,3 @@ describe('Teste do Componente About', () => {
     expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
-
