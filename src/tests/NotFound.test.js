07e1e05890ba9edd439d19e81d75b1d5ae0a1fbc
Levy Araujo <levy4raujo@gmail.com>
2023-02-02 10:58:00 -0300

    4º requisito concluído

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { NotFound } from '../pages';
+
+describe('Teste do Componente NotFound', () => {
+  it('Deve renderizar o componente NotFound', () => {
+    render(<NotFound />);
+
+    const title = screen.getByRole('heading', { level: 2, name: 'Page requested not found' });
+    expect(title).toBeInTheDocument();
+  });
+
+  it('Testando imagem no componente About', () => {
+    render(<NotFound />);
+
+    const img = screen.getByAltText(/Pikachu crying because the page requested was not found/i);
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
